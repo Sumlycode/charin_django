1e124b0ff65a1a0a896896d1255625f1c737ca05
Sumlycode <charin55220012@gmail.com>
2022-07-23 11:18:49 +0700

    vi

--- Blog/views.py
+++ Blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render , HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello Django')
+    return HttpResponse('Charin Phimathai 6412732102')
     
